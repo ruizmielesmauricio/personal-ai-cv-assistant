840870a8424fd1cffbc71cc34a24e442af0c0a61
Mauricio Ruiz <ruizmielesmauricio@gmail.com>
2026-06-04 23:06:13 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,12 +14,7 @@ sys.path.append(str(ROOT_DIR))
 import streamlit as st
 from src.rag_pipeline import build_vector_database, generate_answer
 
-try:
-    import pysqlite3
-    sys.modules["sqlite3"] = pysqlite3
-except ImportError:
-    pass
-    
+
 st.set_page_config(
     page_title="Mauricio AI CV Assistant",
     layout="wide"
